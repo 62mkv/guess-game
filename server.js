05ab5c7593780c23bee80b2151862bda0ef8eb0c
Kirill Marchuk <62mkv@mail.ru>
2016-10-24 22:39:41 +0700

    hosting static content with hapi.js

---
+++ server.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: 'html',
+                index: true
+            }
+        }
+    });
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+
+});
